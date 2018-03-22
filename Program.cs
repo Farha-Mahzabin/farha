8622b08628b4f60d9e02f59b5127a95a6c1ab58f
Farha-Mahzbin <zabin.farha.01@gmail.com>
2018-03-22 10:12:16 +0600

    modified to cse,ru

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MYAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
